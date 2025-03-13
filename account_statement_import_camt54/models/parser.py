dddc7564df023dd8773fbc86739ec5dbdadfe4c1
Emanuel Cino <ecino@compassion.ch>
2025-03-13 15:16:39 +0100

    FIX CAMT.054 statement parsing to correctly extract QR-IBAN

--- account_statement_import_camt54/models/parser.py
+++ account_statement_import_camt54/models/parser.py
@@ -1,5 +1,6 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import re
 
 from odoo import _, models
 from odoo.exceptions import UserError
@@ -114,3 +115,24 @@ class CamtParser(models.AbstractModel):
             # camt54 use amounts only from txdtls
             transaction["amount"] = self.parse_amount(ns, node)
         return super().parse_amount_details_currency(ns, node, transaction)
+
+    def parse_statement(self, ns, node):
+        """In case of a camt54 file, the QR-IBAN to be used as the account_number
+        is found in another place than the IBAN."""
+        result = super().parse_statement(ns, node)
+        re_camt_version = re.compile(
+            r"(^urn:iso:std:iso:20022:tech:xsd:camt.054." r"|^ISO:camt.054.)"
+        )
+        if re_camt_version.search(ns):
+            self.add_value_from_node(
+                ns,
+                node,
+                [
+                    "./ns:Ntry[1]/ns:NtryRef",
+                    "./ns:Acct/ns:Id/ns:IBAN",
+                    "./ns:Acct/ns:Id/ns:Othr/ns:Id",
+                ],
+                result,
+                "account_number",
+            )
+        return result
