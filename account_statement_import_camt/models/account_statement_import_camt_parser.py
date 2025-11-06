ea96a2146da17bf990277dc393a0e83c288bc1a6
davidwul <dwulliamoz@compassion.ch>
2025-11-06 15:43:23 +0100

    Update parser.py
    add Referred Document Information

--- account_statement_import_camt/models/account_statement_import_camt_parser.py
+++ account_statement_import_camt/models/account_statement_import_camt_parser.py
@@ -88,6 +88,14 @@ class AccountStatementImportCamtParser(models.AbstractModel):
         self.add_value_from_node(
             ns,
             node,
+            ["./ns:RmtInf/ns:Strd/ns:RfrdDocInf/ns:Nb"],
+            transaction["narration"],
+            "%s (RmtInf/Strd/RfrdDocInf/Nb)" % _("Referred Document Information"),
+            join_str=" ",
+        )
+        self.add_value_from_node(
+            ns,
+            node,
             "./ns:RmtInf/ns:Strd/ns:AddtlRmtInf",
             transaction["narration"],
             "%s (RmtInf/Strd/AddtlRmtInf)" % _("Additional Remittance Information"),
